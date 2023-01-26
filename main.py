18e0e0eade0171178d662c98024f03cd23d2061f
hace4 <42021756+hace4@users.noreply.github.com>
2023-01-26 21:56:08 +0300

    sxc

--- main.py
+++ main.py
@@ -52,12 +52,12 @@ try:
                 time.sleep(time_limit)
                 result2 = result2.dice.value
                 all_rez2.append(int(result2))
-                db.plus_score(message.from_user.id, result2)
                 if result2 == 64:
                     await bot.send_message(message.chat.id, "ЕЕЕЕЕЕЕЙ ТРИ ТОПОРА ПРИЗ {jack} БАЛЛОВ В КОПИЛКУ".format(jack=jack))
                     db.plus_score(message.from_user.id, jack)
                     all_rez2.append(int(jack))
             all_rez2 = sum(all_rez2)
+            db.plus_score(message.from_user.id, result2)
             await bot.send_message(message.chat.id, 'За эти броски заработал {all_rez}'.format(all_rez=all_rez2))
         else:
             await bot.send_message(message.chat.id,
@@ -79,12 +79,12 @@ try:
                 time.sleep(time_limit)
                 result2 = result2.dice.value
                 all_rez2.append(int(result2))
-                db.plus_score(message.from_user.id, result2)
                 if result2 == 6:
                     await bot.send_message(message.chat.id, "ЕЕЕЕЕЕЕЙ ТОЧНО В ЦЕЛЬ ПОКОРНЫ ЛОВИ 6 БАЛЛА В КОПИЛКУ")
                     db.plus_score(message.from_user.id, result2)
                     all_rez2.append(int(result2))
             all_rez2 = sum(all_rez2)
+            db.plus_score(message.from_user.id, result2)
             await bot.send_message(message.chat.id, 'За эти броски заработал {all_rez}'.format(all_rez=all_rez2))
         else:
             await bot.send_message(message.chat.id,
@@ -107,11 +107,11 @@ try:
                 time.sleep(time_limit)
                 result2 = result2.dice.value
                 all_rez2.append(int(result2))
-                db.plus_score(message.from_user.id, result2)
                 if result2 == 6:
                     await bot.send_message(message.chat.id, "ЕЕЕЕЕЕЕЙ ТОЧНО В ЦЕЬ ПОКОРНЫ ЛОВИ 6 БАЛЛА В КОПИЛКУ")
                     db.plus_score(message.from_user.id, result2)
                     all_rez2.append(int(result2))
+            db.plus_score(message.from_user.id, result2)
             all_rez2 = sum(all_rez2)
             await bot.send_message(message.chat.id, 'За эти броски заработал {all_rez}'.format(all_rez=all_rez2))
         else:
